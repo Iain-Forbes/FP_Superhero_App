391321add40f762608ba9a7050de34435ba77ead
Iain Forbes <i.forbes77@gmail.com>
2021-02-17 10:01:22 +0000

    Server/Client Folders

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+
+import './App.css';
+
+function App() {
+  return (
+   <p>Hi Heroes!</p>
+  );
+}
+
+export default App;
---
+++ server/src/main/java/com/example/superheroApp/SuperheroAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.superheroApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperheroAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperheroAppApplication.class, args);
+	}
+
+}
